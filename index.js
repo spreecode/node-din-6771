f81f0da2f753108aeb7eb73c047636d75062b9b3
Leonid Suprun <mr.slay@gmail.com>
2016-07-30 19:12:21 +0300

    Cleanup code

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ din.prototype.drawFrame = function() {
 
 
 	const X0 = this.stamp.left;
-	const X1 = this.stamp.left + mm2pt(7.6);
+	const X1 = X0 + mm2pt(7.6);
 	const X2 = X1 + mm2pt(26.57);
 	const X3 = X2 + mm2pt(13.22);
 	const X4 = X3 + mm2pt(7.76);
@@ -65,8 +65,8 @@ din.prototype.drawFrame = function() {
 	const rowHeight = mm2pt(4.19);
 
 	const Y0 = this.stamp.top;
-	const Y1 = this.stamp.top + mm2pt(6.44);
-	const Y2 = this.stamp.top + mm2pt(17);
+	const Y1 = Y0 + mm2pt(6.44);
+	const Y2 = Y0 + mm2pt(17);
 	const Y3 = Y2 + rowHeight;
 	const Y4 = Y3 + rowHeight;
 	const Y5 = Y4 + rowHeight;
@@ -77,49 +77,51 @@ din.prototype.drawFrame = function() {
 	const Y10 = Y9 + rowHeight;
 	const Y11 = this.stamp.bottom;
 
-	this.doc.path(`M ${this.stamp.right},${this.stamp.top}
-		L ${this.stamp.left},${this.stamp.top},
-		L ${this.stamp.left},${this.stamp.bottom}`).stroke();
+	this.doc
+		.moveTo(this.stamp.right, this.stamp.top)
+		.lineTo(this.stamp.left, this.stamp.top)
+		.lineTo(this.stamp.left, this.stamp.bottom)
+		.stroke();
 
 	// Horizontal lines
 
-	this.line(X7, Y1, X11, Y1)
-		.line(X0, Y2, X11, Y2)
-		.line(X0, Y3, X7, Y3)
-		.line(X0, Y4, X7, Y4)
-		.line(X0, Y5, X7, Y5)
-		.line(X0, Y6, X7, Y6)
-		.line(X0, Y7, X11, Y7)
-		.line(X0, Y8, X4, Y8)
-		.line(X0, Y9, X4, Y9)
-		.line(X10, Y9, X11, Y9)
-		.line(X0, Y10, X11, Y10);
+	this.line( X7,  Y1, X11,  Y1)
+		.line( X0,  Y2, X11,  Y2)
+		.line( X0,  Y3,  X7,  Y3)
+		.line( X0,  Y4,  X7,  Y4)
+		.line( X0,  Y5,  X7,  Y5)
+		.line( X0,  Y6,  X7,  Y6)
+		.line( X0,  Y7, X11,  Y7)
+		.line( X0,  Y8,  X4,  Y8)
+		.line( X0,  Y9,  X4,  Y9)
+		.line(X10,  Y9, X11,  Y9)
+		.line( X0, Y10, X11, Y10);
 
 	// Vertical lines
 
-	this.line(X1, Y2, X1, Y11)
-		.line(X2, Y2, X2, Y11)
-		.line(X3, Y2, X3, Y11)
-		.line(X4, Y0, X4, Y11)
-		.line(X5, Y2, X5, Y6)
-		.line(X6, Y0, X6, Y7)
-		.line(X7, Y0, X7, Y11)
-		.line(X8, Y10, X8, Y11)
-		.line(X9, Y0, X9, Y1)
-		.line(X10, Y7, X10, Y10)
+	this.line( X1,  Y2,  X1, Y11)
+		.line( X2,  Y2,  X2, Y11)
+		.line( X3,  Y2,  X3, Y11)
+		.line( X4,  Y0,  X4, Y11)
+		.line( X5,  Y2,  X5,  Y6)
+		.line( X6,  Y0,  X6,  Y7)
+		.line( X7,  Y0,  X7, Y11)
+		.line( X8, Y10,  X8, Y11)
+		.line( X9,  Y0,  X9,  Y1)
+		.line(X10,  Y7, X10, Y10);
 
 	return this;
 }
 
 din.prototype.areas = function() {
 	/*
-		A--------------e-------------B
-		|                            |
-		|                            |
-		|                            |
-		b              +-------------C
-		|              |             |
-		E--------------D-------------+
+		A ──────────── e ─────────── B
+		│                            │
+		│                            │
+		│                            │
+		b              ┌──────────── C
+		│              │             │
+		E ──────────── D ────────────┘
 
 		Top area is ABCb described as AC vector.
 		Left area is AeDE described as AD vector.
@@ -136,10 +138,13 @@ din.prototype.line = function(x1, y1, x2, y2) {
 }
 
 din.prototype.drawBorder = function() {
-	this.doc.path(`M ${this.margins.left},${this.margins.top}
-		L ${this.margins.left},${this.margins.bottom}
-		L ${this.margins.right},${this.margins.bottom}
-		L ${this.margins.right},${this.margins.top} z`).stroke();
+	this.doc
+		.moveTo(this.margins.left, this.margins.top)
+		.lineTo(this.margins.left, this.margins.bottom)
+		.lineTo(this.margins.right, this.margins.bottom)
+		.lineTo(this.margins.right, this.margins.top)
+		.lineTo(this.margins.left, this.margins.top)
+		.stroke();
 
 	return this;
 }
@@ -162,5 +167,5 @@ function mm2pt(mm) {
 	const DPI = 72;
 	const MMPI = 25.4;
 
-	return DPI * mm / MMPI;
+	return mm * DPI / MMPI;
 }
